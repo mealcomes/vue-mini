f0a61dd21eb7578b2ee34de59f67ea911c0da6d2
mealcomes <2013213920@qq.com>
2025-06-27 00:48:08 +0800

    fix: dom元素也被转为了reactive对象

--- packages/reactivity/src/reactive.ts
+++ packages/reactivity/src/reactive.ts
@@ -5,6 +5,30 @@ import { ReactiveFlags } from "./constants";
 // 用于记录代理后的结果，以便可以复用
 const reactiveMap = new WeakMap();
 
+enum TargetType {
+    INVALID = 0,
+    COMMON = 1,
+    COLLECTION = 2,
+}
+function targetTypeMap(rawType: string) {
+    switch (rawType) {
+        case 'Object':
+        case 'Array':
+            return TargetType.COMMON
+        case 'Map':
+        case 'Set':
+        case 'WeakMap':
+        case 'WeakSet':
+            return TargetType.COLLECTION
+        default:
+            return TargetType.INVALID
+    }
+}
+function getTargetType(value) {
+    const rawType = Object.prototype.toString.call(value).slice(8, -1);
+    return targetTypeMap(rawType)
+}
+
 function createReactiveObject(target: object) {
     if (!isObject(target)) {
         return target;
@@ -12,6 +36,13 @@ function createReactiveObject(target: object) {
     if (target[ReactiveFlags.IS_REACTIVE]) {
         return target;
     }
+
+    // 处理一些不能被代理的对象，例如dom元素
+    const targetType = getTargetType(target)
+    if (targetType === TargetType.INVALID) {
+        return target
+    }
+
     const existingProxy = reactiveMap.get(target);
     if (existingProxy) {
         return existingProxy;
