5db9df91218624847f11901c4ae18dd080ba25ed
mealcomes <2013213920@qq.com>
2025-06-28 01:23:58 +0800

    refactor: 梳理instance中的成员，修改setupContext构建逻辑

--- packages/runtime-core/src/component.ts
+++ packages/runtime-core/src/component.ts
@@ -22,23 +22,43 @@ export function createComponentInstance(
     const type = vnode.type
 
     const instance = {
-        type,                  // h 函数的第一个参数, 
         // state: {},          // 原本 data 为 state
-        data: {},              // 响应式数据，vue2中的data函数返回的对象
-        render: null,          // render 函数
         vnode,                 // 组件的虚拟节点
+        type,                  // h 函数的第一个参数, 
+        next: null,            // 组件更新时用到的next
         subTree: null,         // 子树
-        isMounted: false,      // 是否挂载完成
         update: null,          // 组件的更新函数
+        render: null,          // render 函数
+        proxy: null,           // 用来代理 props attrs data 方便访问
+        exposed: null,         // 用于暴露组件内部数据的函数
+
+        components: null,
+
+        propsOptions,          // 组件的props，其为defineProps(用户声明的props)
+
+        emit: null,            // 组件的发布事件函数
+
+        data: {},              // 响应式数据，vue2中的data函数返回的对象
         props: {},             // API defineProps的那个props(最终父组件传过来的，其属于propsOptions)
         attrs: {},             // propsOptions - props = attrs
         slots: {},             // 插槽
-        propsOptions,          // 组件的props，其为defineProps(用户声明的props)
-        component: null,
-        proxy: null,           // 用来代理 props attrs data 方便访问
         setupState: {},        // setup函数返回的对象
-        exposed: null,
+
+        isMounted: false,      // 是否挂载完成
+        bm: null,
+        m: null,
+        bu: null,
+        u: null,
+        um: null,
+        bum: null,
     }
+
+    // 通过合成eventName，再找到父组件onEventName属性对应的函数并对其进行调用
+    instance.emit = function (event: string, ...args) {
+        const eventName = `on${event[0].toUpperCase() + event.slice(1)}`;
+        const handler = instance.vnode.props[eventName];
+        handler && handler(...args);
+    };
 
     return instance
 }
@@ -83,20 +103,7 @@ function setupStatefulComponent(instance) {
 
     const { setup } = Component;
     if (setup) {
-        const setupContext = {
-            slots: instance.slots,
-            attrs: instance.attrs,
-            // 通过合成eventName，再找到父组件onEventName属性对应的函数并对其进行调用
-            emit(event: string, ...args) {
-                const eventName = `on${event[0].toUpperCase() + event.slice(1)}`;
-                const handler = instance.vnode.props[eventName];
-                handler && handler(...args);
-            },
-            // 在 instance 上添加对象
-            expose(value) {
-                instance.exposed = value;
-            },
-        }
+        const setupContext = createSetupContext(instance);
         // 设置当前组件实例到全局，从而setup函数能够拿到该实例
         const reset = setCurrentInstance(instance);
         // 传入 setupContext， 见index.html中使用的setup传入的参数
@@ -135,3 +142,17 @@ function finishComponentSetup(instance) {
         console.error(e)
     }
 }
+
+export function createSetupContext(instance) {
+    const expose = exposed => {
+        instance.exposed = exposed || {};
+    };
+
+    return {
+        attrs: instance.attrs,
+        slots: instance.slots,
+        emit: instance.emit,
+        expose,
+    }
+}
+
